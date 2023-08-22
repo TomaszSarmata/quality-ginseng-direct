567c12a716397de593aa02198408185566470794
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-22 13:48:22 +0100

    created a list of products in components dir

---
+++ components/products/product-grid/products.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    title: "Red Korean Ginseng",
+    subtitle: "8000mg Capsules",
+    price: "£9.99",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corrupti numquam, ipsum praesentium perspiciatis deserunt dolorem, dolorum quo recusandae soluta odit quisquam veritatis mollitia perferendis ducimus saepe cumque consectetur eligendi",
+    imgUrl: "/images/",
+  },
+  {
+    title: "White Korean Ginseng",
+    subtitle: "8000mg Capsules",
+    price: "£9.99",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corrupti numquam, ipsum praesentium perspiciatis deserunt dolorem, dolorum quo recusandae soluta odit quisquam veritatis mollitia perferendis ducimus saepe cumque consectetur eligendi",
+    imgUrl: "/images/",
+  },
+  {
+    title: "American Ginseng",
+    subtitle: "8000mg Capsules",
+    price: "£9.99",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corrupti numquam, ipsum praesentium perspiciatis deserunt dolorem, dolorum quo recusandae soluta odit quisquam veritatis mollitia perferendis ducimus saepe cumque consectetur eligendi",
+    imgUrl: "/images/",
+  },
+  {
+    title: "Brasilian Ginseng",
+    subtitle: "8000mg Capsules",
+    price: "£9.99",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corrupti numquam, ipsum praesentium perspiciatis deserunt dolorem, dolorum quo recusandae soluta odit quisquam veritatis mollitia perferendis ducimus saepe cumque consectetur eligendi",
+    imgUrl: "/images/",
+  },
+  {
+    title: "Siberian Ginseng",
+    subtitle: "8000mg Capsules",
+    price: "£9.99",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corrupti numquam, ipsum praesentium perspiciatis deserunt dolorem, dolorum quo recusandae soluta odit quisquam veritatis mollitia perferendis ducimus saepe cumque consectetur eligendi",
+    imgUrl: "/images/",
+  },
+  {
+    title: "Indian Ginseng",
+    subtitle: "8000mg Capsules",
+    price: "£9.99",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis corrupti numquam, ipsum praesentium perspiciatis deserunt dolorem, dolorum quo recusandae soluta odit quisquam veritatis mollitia perferendis ducimus saepe cumque consectetur eligendi",
+    imgUrl: "/images/",
+  },
+];
+
+export default products;
