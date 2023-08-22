6279f7c86c13fc48097266c584f6e0f08ed0839f
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-22 14:28:34 +0100

    added padding

--- components/products/product-grid/index.js
+++ components/products/product-grid/index.js
@@ -4,7 +4,7 @@ import ProductItem from "./product-item";
 export default function ProductGrid() {
   return (
     <div>
-      <div className="grid grid-cols-3 bg-red-100 gap-4">
+      <div className="grid grid-cols-3 bg-red-100 gap-4 p-4">
         {products.map((product) => {
           return <ProductItem product={product}></ProductItem>;
         })}
