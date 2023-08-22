a1a00d46f8b3b6028c1278373e8303fa2a920d5e
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-22 14:50:39 +0100

    took off horizontal padding from Content compnent to improve the layout of the page

--- components/shared/content/index.js
+++ components/shared/content/index.js
@@ -1,3 +1,3 @@
 export default function Content({ children }) {
-  return <div className="w-full max-w-6xl mx-auto py-4 px-4">{children}</div>;
+  return <div className="w-full max-w-6xl mx-auto py-4">{children}</div>;
 }
