701735d01ce9a5aabd88fdbe7f93513681efc797
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-22 14:11:47 +0100

    added styling to product-item

--- components/products/product-grid/product-item.js
+++ components/products/product-grid/product-item.js
@@ -1,9 +1,13 @@
 export default function ProductItem({ product }) {
   return (
-    <div className="flex flex-col bg-blue-100">
-      <div>{product.title}</div>
-      <div>{product.subtitle}</div>
-      <div>Price: {product.price}</div>
+    <div className="flex flex-col bg-white text-center p-4 rounded-md">
+      <img
+        src="/images/Acerola Cherry 8,000mg 30 Capsules front-1.jpg"
+        alt=""
+      />
+      <h2 className="text-2xl">{product.title}</h2>
+      <h3>{product.subtitle}</h3>
+      <h3>Price: {product.price}</h3>
     </div>
   );
 }
