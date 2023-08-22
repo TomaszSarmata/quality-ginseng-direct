e8f31290f6ededdc798d688966899e7a543147de
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-22 14:20:21 +0100

    created Header, Footer, and Navbar files to implement later in Products.js page

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,15 @@
 import ProductGrid from "@/components/products/product-grid";
+import Footer from "@/components/shared/footer";
+import Header from "@/components/shared/header";
+import NavBar from "@/components/shared/nav-bar";
 
 export default function ProductPage() {
   return (
     <div>
+      <NavBar></NavBar>
+      <Header></Header>
       <ProductGrid></ProductGrid>
+      <Footer></Footer>
     </div>
   );
 }
