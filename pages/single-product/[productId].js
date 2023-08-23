2d0c83ac14519ac89e486ce69176370841a4ce74
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-23 11:23:28 +0100

    implemented adynamic product page to display a particular item for the user with the product image and description

--- pages/single-product/[productId].js
+++ pages/single-product/[productId].js
@@ -1,16 +1,63 @@
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export default function SingleProduct() {
-  const [singleBook, setSingleBook] = useState("not rendered");
+  const [singleProduct, setSingleProduct] = useState(null);
+  const router = useRouter();
+  const productId = router.query.productId;
 
   useEffect(() => {
-    getSingleProduct();
-  });
+    if (!productId) return;
+    getSingleProduct(productId);
+  }, [productId]);
 
-  const getSingleProduct = async () => {
-    const res = await fetch(`/api/books`);
+  const getSingleProduct = async (id) => {
+    const res = await fetch(`/api/product-by-id?id=${id}`);
     const data = await res.json();
-    setSingleBook(data);
+    const { product } = data;
+    setSingleProduct(product);
   };
-  return <div>Single Product</div>;
+  console.log(singleProduct);
+
+  if (!singleProduct) {
+    return <div>loading...</div>;
+  }
+
+  return (
+    <div className="w-full max-w-6xl mx-auto">
+      <div className="w-full flex flex-row gap-x-2">
+        <div className="w-5/12 bg-red-100 p-4">
+          <img src={singleProduct.imgUrl} alt="" />
+        </div>
+        <div className="w-7/12 bg-blue-100 p-4 flex flex-row gap-x-4">
+          <div className="w-6/12 p-4 bg-green-100">
+            <div>{singleProduct.title}</div>
+            <div>{singleProduct.description}</div>
+          </div>
+          <div className="w-6/12 p-4 bg-pink-100">
+            <div>Benefits</div>
+            <div>
+              <ul className="list-disc">
+                <li>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit
+                </li>
+                <li>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit
+                </li>
+                <li>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit
+                </li>
+                <li>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit
+                </li>
+                <li>
+                  Lorem ipsum, dolor sit amet consectetur adipisicing elit
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
