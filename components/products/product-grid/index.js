e48e3d0028852c188dea45e2a1cb123ad4b8a254
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-23 10:25:09 +0100

    added key property to the map( )

--- components/products/product-grid/index.js
+++ components/products/product-grid/index.js
@@ -16,8 +16,8 @@ export default function ProductGrid() {
   return (
     <div>
       <div className="grid grid-cols-3 bg-red-100 gap-4 p-4">
-        {products.map((product) => {
-          return <ProductItem product={product}></ProductItem>;
+        {products.map((product, index) => {
+          return <ProductItem key={index} product={product}></ProductItem>;
         })}
       </div>
     </div>
