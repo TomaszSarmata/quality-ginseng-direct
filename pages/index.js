20059c3347d995cc399f66a2bcdec7e0958da7b2
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-23 14:09:02 +0100

    created new divs on the homepage to display background images

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,19 @@
+import NavBar from "@/components/shared/nav-bar";
+
 export default function Home() {
-  return <div className="bg-red-100">Qualtiy Ginseng Direct</div>;
+  return (
+    <div>
+      <div>
+        <NavBar></NavBar>
+        <div className="w-full">
+          <div className="flex flex-row">
+            <div className="w-4/12 bg-image h-96"></div>
+            <div className="w-4/12"></div>
+            <img src="/images/banner1.webp" className="w-4/12"></img>
+          </div>
+        </div>
+      </div>
+      <div className="bg-image-test h-96"></div>
+    </div>
+  );
 }
