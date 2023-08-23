d4650393439a5226851af566db9b9bbeec175b32
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-23 11:24:17 +0100

    changed the src from hardcoded link to a dynamic one

--- components/products/product-grid/product-item.js
+++ components/products/product-grid/product-item.js
@@ -1,13 +1,12 @@
 export default function ProductItem({ product }) {
   return (
     <div className="flex flex-col bg-white text-center p-4 rounded-md">
-      <img
-        src="/images/Acerola Cherry 8,000mg 30 Capsules front-1.jpg"
-        alt=""
-      />
-      <h2 className="text-2xl">{product.title}</h2>
-      <h3>{product.subtitle}</h3>
-      <h3>Price: {product.price}</h3>
+      <a href={`/single-product/${product.id}`}>
+        <img src={product.imgUrl} alt="" />
+        <h2 className="text-2xl">{product.title}</h2>
+        <h3>{product.subtitle}</h3>
+        <h3>Price: {product.price}</h3>
+      </a>
     </div>
   );
 }
