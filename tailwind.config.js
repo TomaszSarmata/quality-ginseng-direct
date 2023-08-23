823c15812198c72de7569953c5f4e3fc95eda0d1
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-23 13:21:26 +0100

    tried creating a background image???

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
-
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
- 
-    
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        banner1: "url('/images/banner1.webp')",
+      },
+    },
   },
   plugins: [],
-}
+};
