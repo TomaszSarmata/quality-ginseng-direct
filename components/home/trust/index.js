b35f9e0d1722b414b8b3de6df2eb877146de6d23
Tomasz <98025536+TomaszSarmata@users.noreply.github.com>
2023-08-24 15:06:51 +0100

    created TrustDisplay component and implemented it on the home page

---
+++ components/home/trust/index.js
@@ -0,0 +1,64 @@
+export default function TrustDisplay() {
+  return (
+    <div className="flex flex-row items-center justify-center bg-gray-50 my-10">
+      <div className="w-6/12">
+        <img src="/images/content.webp" alt="" />
+      </div>
+      <div className="w-6-12 pl-10 ">
+        <ol className="flex flex-col gap-y-10">
+          <li className="text-4xl">Supplements You Can Trust</li>
+          <li className="flex gap-x-3  ">
+            <div>
+              <img
+                className="h-9 self-start"
+                src="/images/QA_100x.webp"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4 className="text-xl font-medium mb-1">Qeen's Award Winning</h4>
+              <p>
+                The only vitamin company to receive the Queen's Award for
+                Innovation TWICE
+              </p>
+            </div>
+          </li>
+          <li className="flex gap-x-3">
+            <div>
+              <img
+                className="h-9"
+                src="/images/50_years_innovation_100x.avif"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4 className="text-xl font-medium mb-1">
+                50 Years of Innovation
+              </h4>
+              <p>
+                We've been pushing the boundaries for 50 years to help your
+                family be at your best.
+              </p>
+            </div>
+          </li>
+          <li className="flex gap-x-3 ">
+            <div>
+              <img
+                className="h-9"
+                src="/images/backed_by_science_100x.webp"
+                alt=""
+              />
+            </div>
+            <div>
+              <h4 className="text-xl font-medium mb-1">Backed by Science</h4>
+              <p>
+                No other UK vitamin company has such a diverse range of
+                published research to support its products.
+              </p>
+            </div>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
+}
